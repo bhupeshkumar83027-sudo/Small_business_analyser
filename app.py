4e116b7ba0c0e0e8293a8324448d59cfc275d3f8
Bhupesh Kumar <bhupeshkumar83027@gmail.com>
2026-03-23 22:22:39 +0530

    Remove unnecessary session state initialization

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ if "logged_in" not in st.session_state:
     st.session_state.dashboard_data = None
     st.session_state.role = None
     st.session_state.username = None
-cddccd
 if st.session_state.logged_in:
     if st.session_state.role and st.session_state.role.lower() == "admin":
         admin_dashboard()
